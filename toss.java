14f47ada033f2e17312e13dc1a8a75390ab6f3bb
vemayaswanth <125953227+vemayaswanth@users.noreply.github.com>
2023-03-27 12:06:32 +0530

    Create toss.java

---
+++ toss.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+class Toss {
+   public String chanceFunc() {
+      Random r = new Random();
+      int chance = r.nextInt(2);
+      if (chance == 1) {
+         return"tails";
+      } else {
+         return"heads";
+      }
+   }
+}
+public class Demo {
+   public static void main(String[] args) {
+      Toss t = new Toss();
+      int heads = 0;
+      int tails = 0;
+      int chances = 10;
+      for (int i = 1; i<= chances; i++) {
+         if (t.chanceFunc().equals("tails")) {
+            tails++;
+         } else {
+            heads++;
+         }
+      }
+      System.out.println("Chances = " + chances);
+      System.out.println("Heads: " + heads);
+      System.out.println("Tails: " + tails);
+   }
+}
